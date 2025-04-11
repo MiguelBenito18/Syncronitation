fa754ddcc9b9282a8ed0be3595d287bbe4d9cdc2
Miguel Benito <879608@unizar.es>
2025-04-11 16:14:45 +0200

    Prueba

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#define NIP_SEED 000000
+#define N_SHAPES 17890
+#define MAX_STRING_LENGTH 256
+#define CITY_CENTER ((Point){.x = 676452.6, .y = 4613323})
+#define MAX_DISTANCE_CITY 3000
+#define GAUSSIAN_FACTOR 0.00005
+#define MAX_STEPS 100000
+
+typedef struct Point {
+    double x;
+    double y;
+} Point;
 
